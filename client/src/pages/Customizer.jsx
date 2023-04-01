cc0cecc50f24d32aa4602d92892f98b8c4e07b26
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2023-04-02 03:40:56 +0530

    changed backend url

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -57,7 +57,7 @@ const Customizer = () => {
     try {
       //call our backend to generate an AI image
       setGeneratingImg(true);
-      const response = await fetch("http://localhost:8080/api/v1/dalle", {
+      const response = await fetch("https://try-it.onrender.com/api/v1/dalle", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
